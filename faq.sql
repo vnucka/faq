01622b47d26baeaab37e170d3670d4a2f42df441
vnuck <vnucka@mail.ru>
2017-08-21 19:24:21 +1000

    Тестовые записи

--- faq.sql
+++ faq.sql
@@ -23,19 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `answers`
---
-
-CREATE TABLE `answers` (
-  `id` int(10) NOT NULL,
-  `answer` varchar(3000) NOT NULL COMMENT 'ответ',
-  `user_id` int(10) NOT NULL COMMENT 'id отвечающего',
-  `question_id` int(10) NOT NULL COMMENT 'id вопроса',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата ответа',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
 -- Дамп данных таблицы `answers`
 --
 
@@ -45,52 +32,6 @@ INSERT INTO `answers` (`id`, `answer`, `user_id`, `question_id`, `created_at`, `
 (5, 'Solid State Disk', 2, 5, '2017-08-18 05:16:14', '2017-08-18 05:16:14');
 
 -- --------------------------------------------------------
-
---
--- Структура таблицы `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `password_resets`
---
-
-INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
-('borya@mail.ru', '$2y$10$8ol.7kyQQt8GPuiuPwvl7uX9gnYjZCMpOHnernJzxZDtgL.2rMVzG', '2017-08-19 03:15:11');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `questions`
---
-
-CREATE TABLE `questions` (
-  `id` int(10) NOT NULL,
-  `theme_id` int(10) NOT NULL COMMENT 'id темы',
-  `name` varchar(100) NOT NULL COMMENT 'Заголовок вопроса',
-  `text` varchar(3000) NOT NULL COMMENT 'Текст вопроса',
-  `user_id` int(10) NOT NULL COMMENT 'id автора',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата создания вопроса',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `moderate` varchar(30) NOT NULL DEFAULT 'moderate' COMMENT 'Модерация (true, false, moderate)'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
 -- Дамп данных таблицы `questions`
@@ -108,17 +49,6 @@ INSERT INTO `questions` (`id`, `theme_id`, `name`, `text`, `user_id`, `created_a
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `themes`
---
-
-CREATE TABLE `themes` (
-  `id` int(10) NOT NULL,
-  `name` varchar(50) NOT NULL COMMENT 'Тема',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-
---
 -- Дамп данных таблицы `themes`
 --
 
@@ -130,95 +60,13 @@ INSERT INTO `themes` (`id`, `name`, `created_at`, `updated_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `users`
---
-
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `role` varchar(30) CHARACTER SET utf8 NOT NULL DEFAULT 'user' COMMENT 'Роль (admin, moderator, user)'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `role`) VALUES
-(1, 'Александр', 'vnucka@mail.ru', '$2y$10$kMaHztI2Zvb10eA7x3nPdO8BajTw6xAk8ptnlQks0zZG5fLqMn1bu', 'I69EvOSYTVyucFRtLxZTWSjBXejrKnB1t4RGcDDjaUkM9MBtifJobIbBGJwU', '2017-08-07 02:16:34', '2017-08-07 02:16:34', 'admin'),
+(5, 'Александр', 'vnucka@mail.ru', '$2y$10$kMaHztI2Zvb10eA7x3nPdO8BajTw6xAk8ptnlQks0zZG5fLqMn1bu', 'I69EvOSYTVyucFRtLxZTWSjBXejrKnB1t4RGcDDjaUkM9MBtifJobIbBGJwU', '2017-08-07 02:16:34', '2017-08-07 02:16:34', 'admin'),
 (2, 'Василий Пупкин', 'vasya@mail.ru', '$2y$10$KWLv4bEUaVaZw8RCH7Zqr.uU7iRaZL6/wlcpnSPpO9Tml6uQHxTxa', 'wAD4KCZJBN30bpV9yX8wfJedhveiZYSPSatlSLQHZSineTbaQinTJdeMypX1', '2017-08-12 23:55:48', '2017-08-20 18:13:45', 'user'),
 (3, 'Анаис Брахимов', 'brachim@mail.ru', '$2y$10$pk4dblmOJB2P7xoGbpQ1qOOe5kG8YHM.NI5Cyraa9FsBYZ1YafYBW', 'xSyup8Bi1MtpjaV2zdIWIxnZlktAB9SlWuudcPE7tiRnouZpwn2QrtRDEHCh', '2017-08-13 02:18:07', '2017-08-13 02:18:07', 'user'),
 (4, 'Анатонина', 'vnuchok@mail.ru', '$2y$10$QHAVk1./gk3llR4kU7l8N.OHGh78rpx4g83VNd7jxI/9/5u6ejciK', 'ilFO5D3mIwY71DdZHQz3JFrpYsoZzyw9HzrXfOf5jZbMwYgX9YO69OUtERDr', '2017-08-16 20:10:35', '2017-08-20 04:09:43', 'moderator'),
 (21, 'Борис', 'borya@mail.ru', '$2y$10$kmkwmKrzB4uLcTWqWO032ecN11of8KzGx/xmqElBFMp2q9LGO2Z1m', 'oQ7FdWa47spKSOz4WNEfSh7v1c6pIRdCNpCuReYnwZUPmI9ya02nZMAmtyeB', '2017-08-19 06:08:03', '2017-08-19 06:08:03', 'user'),
 (22, 'Юлия', 'yula@mail.ru', '$2y$10$V9O7/Hw2BtJ0yM/RZt/exuq8HyYMCE5BPGgCbpsQ2LINZCX/kd3aG', 'e9zHjzZZWst5Xw043gyFYmhjVbAPNSqM2XZpr7jZ0iMIpeX46UOmusPaNxW8', '2017-08-19 21:20:21', '2017-08-19 21:20:21', 'user');
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `answers`
---
-ALTER TABLE `answers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `questions`
---
-ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `themes`
---
-ALTER TABLE `themes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `answers`
---
-ALTER TABLE `answers`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT для таблицы `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `questions`
---
-ALTER TABLE `questions`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
---
--- AUTO_INCREMENT для таблицы `themes`
---
-ALTER TABLE `themes`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT для таблицы `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
